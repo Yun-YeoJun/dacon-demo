95ea47545bc28ffb01fd3ae4eb658583aad65f6c
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2026-02-08 23:52:56 +0900

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/v1': 'http://127.0.0.1:8000',
       '/share': 'http://127.0.0.1:8000'
